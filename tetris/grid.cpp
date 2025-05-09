8359b4b4c6a8f74dabbf055948421baf297f607a
joyur98 <kcruyoj7@gmail.com>
2025-05-09 17:48:05 +0545

    Initialize() was defined

--- tetris/grid.cpp
+++ tetris/grid.cpp
@@ -5,3 +5,11 @@ Grid::Grid() {
 	num_cols = 15;
 	cell_size = 25;
 }
+
+void Grid::Initialize() {
+	for (int i = 0; i < num_rows; i++) {
+		for (int j = 0; j < num_cols; j++) {
+			grid[i][j] = 0;
+		}
+	}
+}
