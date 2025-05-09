147733773f938975d8072f4c51d4c6eb3172b421
joyur98 <kcruyoj7@gmail.com>
2025-05-09 18:07:53 +0545

    draw method was defined to draw the grids in the window

--- tetris/grid.cpp
+++ tetris/grid.cpp
@@ -5,6 +5,8 @@ Grid::Grid() {
 	num_rows = 30;
 	num_cols = 15;
 	cell_size = 25;
+	Initialize();
+	colors = GetCellColors();
 }
 
 void Grid::Initialize() {
@@ -47,4 +49,13 @@ std::vector<Color> Grid::GetCellColors() {
 		cyan,
 		blue
 	};
+}
+
+void Grid::Draw() {
+	//i is the rows and j is the columns so x axis = j and y axis = i
+	for (int i = 0; i < num_rows; i++) {
+		for (int j = 0; j < num_cols; j++) {
+			DrawRectangle(j * cell_size+1, i * cell_size + 1, cell_size - 1, cell_size - 1, colors[grid[i][j]]);
+		}
+	}
 }
