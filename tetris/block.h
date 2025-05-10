ad8823e8d6e8300c3351b05758f4567fa441a909
joyur98 <kcruyoj7@gmail.com>
2025-05-10 02:55:51 -0700

    some spelling mistake

--- tetris/block.h
+++ tetris/block.h
@@ -10,7 +10,7 @@ public:
 	//id is the id of the block
 	int id;
 
-	//cells is a hashmap which contains the id and the position of the vector in rotation states
+	//cells is a hashmap which contains the id and the position of the block in rotation states
 	std::map<int, std::vector<Position>> cells;
 
 private:
