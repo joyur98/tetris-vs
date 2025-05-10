7cd2a3189be38423b31059330b74343d129d7d80
joyur98 <kcruyoj7@gmail.com>
2025-05-10 18:39:49 +0545

    the function is defined in the block cpp file

--- tetris/block.cpp
+++ tetris/block.cpp
@@ -5,6 +5,8 @@ Block::Block(){
 	cell_size = 25;
 	rotation_state = 0;
 	colors = GetCellColors();
+	row_offset = 0;
+	col_offset = 0;
 }
 
 void Block::Draw() {
@@ -15,4 +17,21 @@ void Block::Draw() {
 		DrawRectangle(item.col * cell_size + 1, item.row * cell_size + 1, cell_size - 1, cell_size - 1, colors[id]);
 	}
 
+}
+
+void Block::Move(int rows, int cols) {
+	row_offset += rows;
+	col_offset += cols;
+};
+
+std::vector<Position> Block::GetCellPosition() {
+	std::vector<Position> tiles = cells[rotation_state];
+	std::vector<Position> moved_tiles;
+
+	for (Position item : tiles) {
+		Position new_pos = Position(item.row + +row_offset, item.col + col_offset);
+		moved_tiles.push_back(new_pos);
+	}
+
+	return moved_tiles;
 }
