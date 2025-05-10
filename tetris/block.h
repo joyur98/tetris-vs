3bec3e2ba61d81202fe65899935e269f44776a19
joyur98 <kcruyoj7@gmail.com>
2025-05-10 15:40:00 +0545

    added new code to the block header file which stores the blocks and their rotation states

--- tetris/block.h
+++ tetris/block.h
@@ -1,10 +1,23 @@
 #pragma once
-
+#include <vector>
+#include <map>
+#include "position.h"
 
 class Block {
 public:
 	Block();
+
+	//id is the id of the block
 	int id;
+
+	//cells is a hashmap which contains the id and the position of the vector in rotation states
+	std::map<int, std::vector<Position>> cells;
+
+private:
+	int cell_size;
+	int rotation_state;
+
+
 
 private:
 
