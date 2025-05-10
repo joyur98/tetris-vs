befdeea677e5ea22cc5c0e3877e84dc857cd7045
joyur98 <kcruyoj7@gmail.com>
2025-05-10 17:14:41 +0545

    the LBlock class was created to draw the Lblock

---
+++ tetris/block.cpp
@@ -0,0 +1,17 @@
+#include "block.h"
+#include "raylib.h"
+
+Block::Block(){
+	cell_size = 25;
+	rotation_state = 0;
+	colors = GetCellColors();
+}
+
+void Block::Draw() {
+	std::vector<Position> tiles = cells[rotation_state];
+
+	for (Position item : tiles) {
+		DrawRectangle(item.col * cell_size + 1, item.row * cell_size + 1, cell_size - 1, cell_size - 1, colors[id]);
+	}
+
+}
--- tetris/grid.h
+++ tetris/grid.h
@@ -12,9 +12,6 @@ public:
 	int grid[30][15];
 private:
 
-
-
-
 	int num_rows;
 	int num_cols;
 	int cell_size;
