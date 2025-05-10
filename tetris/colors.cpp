a11be33b0228ac3e96a0c3958c73dabf5c77fa57
joyur98 <kcruyoj7@gmail.com>
2025-05-10 16:06:17 +0545

    just corrected some syntax

--- tetris/colors.cpp
+++ tetris/colors.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 
 //the rgba values of the colors
-extern Color darkGrey = { 26, 31, 40, 255 };
-extern Color green = { 47, 230, 23, 255 };
-extern Color orange = { 226, 116, 17, 255 };
-extern Color red = { 232, 18, 18, 255 };
-extern Color yellow = { 237, 234, 4, 255 };
-extern Color purple = { 166, 0, 247, 255 };
-extern Color cyan = { 21, 204, 209, 255 };
-extern Color blue = { 13, 64, 216, 255 };
+const Color darkGrey = { 26, 31, 40, 255 };
+const Color green = { 47, 230, 23, 255 };
+const Color orange = { 226, 116, 17, 255 };
+const Color red = { 232, 18, 18, 255 };
+const Color yellow = { 237, 234, 4, 255 };
+const Color purple = { 166, 0, 247, 255 };
+const Color cyan = { 21, 204, 209, 255 };
+const Color blue = { 13, 64, 216, 255 };
 
 
 //now returning the vector of colors
