f19a7f29c75bae448fa49a5a76cf4cb42000ca36
joyur98 <kcruyoj7@gmail.com>
2025-05-10 18:40:12 +0545

    New blocks are added

--- tetris/blocks.cpp
+++ tetris/blocks.cpp
@@ -13,3 +13,59 @@ public:
 		cells[3] = { Position(0, 0), Position(0,1), Position(1, 1), Position(2, 1) };
 	}
 };
+
+class IBlock : public Block {
+public:
+	IBlock() {
+		id = 2;
+		cells[0] = { Position(1,0), Position(1, 1), Position(1, 2), Position(1, 3) };
+		cells[1] = { Position(0,2), Position(1, 2), Position(2, 2), Position(3, 2) };
+		cells[2] = { Position(2,0), Position(2, 1), Position(2, 2), Position(2, 3) };
+		cells[3] = { Position(0,1), Position(1, 1), Position(2,1), Position(3,1) };
+	}
+};
+
+class SBlock : public Block {
+public:
+	SBlock() {
+		id = 3;
+		cells[0] = { Position(0,1), Position(0, 2), Position(1, 0), Position(1, 1) };
+		cells[1] = { Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2) };
+		cells[2] = { Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1) };
+		cells[3] = { Position(0, 0), Position(1,0), Position(1, 1), Position(2,1) };
+	}
+};
+
+class TBlock : public Block {
+public:
+	TBlock() {
+		id = 4;
+		cells[0] = { Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2) };
+		cells[1] = { Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1) };
+		cells[2] = { Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1) };
+		cells[3] = { Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1) };
+	}
+};
+
+class ZBlock : public Block {
+public:
+	ZBlock() {
+		id = 5;
+		cells[0] = { Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2) };
+		cells[1] = { Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1) };
+		cells[2] = { Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2) };
+		cells[3] = { Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0) };
+	}
+};
+
+class OBlock : public Block {
+public:
+	OBlock() {
+		id = 6;
+		cells[0] = { Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1) };
+		cells[1] = { Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1) };
+		cells[2] = { Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1) };
+		cells[3] = { Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1) };
+	}
+};
+
