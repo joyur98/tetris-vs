abcd06aa5ae8e4931f8a89aeafc4e3c8089d9442
joyur98 <kcruyoj7@gmail.com>
2025-05-10 15:56:24 +0545

    the variables are given a value in colors cpp file

---
+++ tetris/colors.cpp
@@ -0,0 +1,11 @@
+#include "colors.h"
+
+//the rgba values of the colors
+extern Color darkGrey = { 26, 31, 40, 255 };
+extern Color green = { 47, 230, 23, 255 };
+extern Color orange = { 226, 116, 17, 255 };
+extern Color red = { 232, 18, 18, 255 };
+extern Color yellow = { 237, 234, 4, 255 };
+extern Color purple = { 166, 0, 247, 255 };
+extern Color cyan = { 21, 204, 209, 255 };
+extern Color blue = { 13, 64, 216, 255 };
